5a52ddd38731dc2e852954cea471adfc3826e6df
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-22 15:17:30 -0500

    Add Time class definition with constructors and methods

--- Time.h
+++ Time.h
@@ -1,1 +1,25 @@
+#ifndef TIME_H
+#define TIME_H
 
+#include <iostream>
+using namespace std;
+
+class Time
+{
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
+public:
+    Time();
+    Time(int h, int m, int s);
+
+    int getHours() const;
+    int getMinutes() const;
+    int getSeconds() const;
+
+    friend ostream& operator<<(ostream& out, const Time& t);
+};
+
+#endif
